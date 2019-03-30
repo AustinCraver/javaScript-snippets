eb82bb932ee4dfa7fc6710dadd71486b7ae8b2c5
Austin C <36517066+AustinCraver@users.noreply.github.com>
2019-03-29 22:05:17 -0500

    Create zombieSnowCreature.js
    For project on Khan Academy

---
+++ zombieSnowCreature.js
@@ -0,0 +1,39 @@
+// Radioactive Snow
+textSize(20);
+fill(255, 0, 0);
+text("Thye\'re coming to get you Barbara!", 44, 40);
+
+// basic right arm (left side of canvas)
+stroke(26, 2, 2);
+fill(5, 250, 62);
+rect(65, 169, 151, 14, 5);
+
+// simple snowman
+fill(12, 245, 47);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+
+// red eyes
+noStroke();
+fill(255, 0, 0);
+ellipse(180, 110, 16, 22);
+ellipse(211, 110, 16, 22);
+
+//black pupils
+fill(0, 0, 0);
+ellipse(180, 110, 3, 16);
+ellipse(211, 110, 3, 16);
+
+// mouth
+noStroke();
+fill(0, 0, 0);
+ellipse(196, 140, 40, 12);
+
+// basic right arm (left side of canvas)
+stroke(26, 2, 2);
+fill(5, 250, 62);
+rect(98, 188, 151, 14, 5);
+
+
+
