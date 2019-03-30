4dfdf5925d35c9ad8175312c7dc8570c4161ea5f
Austin C <36517066+AustinCraver@users.noreply.github.com>
2019-03-29 22:31:34 -0500

    Create alienCreature.js

---
+++ alienCreature.js
@@ -0,0 +1,36 @@
+background(255, 0, 0);
+
+// hair of some kind
+fill(0, 0, 255);
+ellipse(200, 110, 121, 106);
+
+// body
+fill(0, 0, 250);
+ellipse(200, 303, 46, 162);
+
+// right arm-like thing
+triangle(223, 298, 368, 219, 217, 246);
+
+// left arm-like thing
+triangle(176, 298, 16, 219, 183, 246);
+
+// face-like shape
+fill(255, 255, 0);
+ellipse(200, 180, 100, 108);
+
+// eyes
+fill(0, 0, 0);
+ellipse(169, 169, 22, 12);
+ellipse(199, 157, 22, 12);
+ellipse(226, 169, 22, 12);
+
+// reds of the eyes
+fill(255, 5, 5);
+ellipse(169, 169, 10, 12);
+ellipse(199, 157, 10, 12);
+ellipse(226, 169, 10, 12);
+
+// emotion-less mouth
+line(175, 208, 225, 208);
+
+
